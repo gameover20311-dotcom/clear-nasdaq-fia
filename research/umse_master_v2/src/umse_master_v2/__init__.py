60ab5df7a596e7f0292fa7f1135488ba9f8f1442
gameover20311-dotcom <gameover20311@gmail.com>
2026-09-12 16:10:07 +0300

    Export V2 survival, null and transport research modules

--- research/umse_master_v2/src/umse_master_v2/__init__.py
+++ research/umse_master_v2/src/umse_master_v2/__init__.py
@@ -12,8 +12,12 @@ from .contracts import (
     QueueSurvivalReport,
     Side,
 )
+from .cross_scale_transport import CrossScaleTransport, TransportConfig, assess_transport
 from .information_velocity import InformationVelocityReport, TimedShock, estimate_information_velocity
+from .leadlag_null import LeadLagEvidence, circular_shift_leadlag_evidence
+from .model_competition import ComplexityCompetition, compare_predictive_complexity
 from .orderbook_memory import OrderBookMemory, estimate_orderbook_memory
+from .queue_hazard import QueueLifetimeDiagnostics, SurvivalPoint, kaplan_meier_queue_lifetime
 from .queue_survival import reconstruct_queue_survival
 from .resistance_field import ResistanceConfig, ResistanceField, estimate_resistance_field
 
@@ -27,10 +31,20 @@ __all__ = [
     "TimedShock",
     "InformationVelocityReport",
     "estimate_information_velocity",
+    "LeadLagEvidence",
+    "circular_shift_leadlag_evidence",
     "OrderBookMemory",
     "estimate_orderbook_memory",
     "reconstruct_queue_survival",
+    "QueueLifetimeDiagnostics",
+    "SurvivalPoint",
+    "kaplan_meier_queue_lifetime",
     "ResistanceConfig",
     "ResistanceField",
     "estimate_resistance_field",
+    "TransportConfig",
+    "CrossScaleTransport",
+    "assess_transport",
+    "ComplexityCompetition",
+    "compare_predictive_complexity",
 ]
